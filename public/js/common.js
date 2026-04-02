7519155327238d28089370721a95befa88815031
tsuge <tsuge@mh-work.com>
2026-04-02 19:13:42 +0900

    ページ増やしてみた

--- public/js/common.js
+++ public/js/common.js
@@ -1,7 +1,7 @@
-const doc = document.querySelector('.js-test-btn');
+const $doc = document.querySelector('.js-test-btn');
 
-if (doc) {
-  doc.addEventListener('click', function () {
-    doc.textContent = 'Clicked!';
+if ($doc) {
+  $doc.addEventListener('click', function () {
+    alert('クリックされました！');
   });
 }
